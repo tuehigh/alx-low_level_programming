b1f2f152d70c5c035f7bf8db69b06780e3f72adf
Clinton <c0772323281@gmail.com>
2023-06-15 13:54:40 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - printing in a program that does not utilise printf
+ * Return:0(progect pragram works)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

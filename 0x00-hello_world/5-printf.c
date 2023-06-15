fc2d83369266a19c10c479260fd8a401849e63fd
Clinton <c0772323281@gmail.com>
2023-06-15 14:12:22 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints utilising printf
+ * return:0(succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

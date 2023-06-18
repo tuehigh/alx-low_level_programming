8e62f6ebdf89a681945410cc8c0d6c0eafdab835
Clinton <c0772323281@gmail.com>
2023-06-18 12:48:22 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * miain - Entry point
+ *
+ * Return:  0(success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')
+		{putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);

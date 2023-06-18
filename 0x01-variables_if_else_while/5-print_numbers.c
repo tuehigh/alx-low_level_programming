8266f622c1a4308fc76c1de6bd8235aa39323c6e
Clinton <c0772323281@gmail.com>
2023-06-18 13:20:07 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while
+		(i <= 9)
+		{
+			putchar(i);
+			i++;
+		}
+	return (0);
+}

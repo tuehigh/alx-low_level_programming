04dce8206b1aa2342a89bf2f1d62b1a0dc687b7a
tuehigh <c0772323281@gmail.com>
2023-09-04 09:47:21 -0700

    update 2

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,4 @@
-#ifdef _MAIN_H_
+#ifndef _MAIN_H_
 #define _MAIN_H_
 int _putchar(char c);
 int _islower(int c);
@@ -20,5 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif

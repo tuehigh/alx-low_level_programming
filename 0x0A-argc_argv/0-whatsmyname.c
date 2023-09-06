b108a8518b346bd5216fd44d18510be462e0c0a7
tuehigh <c0772323281@gmail.com>
2023-09-06 05:01:41 -0700

    added documentation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include <stdlib.h>
-int main(int argc, char **argv)
+/**
+ * main - prints its own name
+ *
+ * Return: 0 Always
+ */
+int main(__attribute__((unused))int argc, char **argv)
 {
         printf("%s\n", argv[0]);
         exit(EXIT_SUCCESS);

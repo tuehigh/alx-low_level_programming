60b0e1adacdfc5c3afcb1d59250a3caaccd9a077
tuehigh <c0772323281@gmail.com>
2023-09-06 05:21:37 -0700

    count parameters

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * main - prints the number of command line arguments
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 Always
+ */
+int main(int argc, __attribute__((unused))char** argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
